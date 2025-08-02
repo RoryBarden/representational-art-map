ad582ed6b31394d2f59603be224e19e22cebec1e
Rory Barden <rorybarden@gmail.com>
2025-08-02 05:23:16 +0100

    added couple more artworks (first photo :D), altered zoom_start value

--- map.py
+++ map.py
@@ -3,7 +3,7 @@ import pandas
 
 # will probably have one map with both photos and paintings shown
 # hopefully will add a bunch of filters (i.e. show only photos from 1921-1964, etc.)
-main_map = folium.Map(location=(0, 0), zoom_start=3)
+main_map = folium.Map(location=(0, 0), zoom_start=2)
 
 artworks = pandas.read_csv("artworks.csv")
 
